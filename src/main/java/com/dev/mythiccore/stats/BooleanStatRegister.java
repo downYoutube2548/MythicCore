03fe50fe0a8728017312cd86717517f31ddff44f
downYotube2548 <maneeohooho3790@gmail.com>
2023-12-08 15:22:10 +0700

    -

--- src/main/java/com/dev/mythiccore/stats/BooleanStatRegister.java
+++ src/main/java/com/dev/mythiccore/stats/BooleanStatRegister.java
@@ -4,9 +4,6 @@ import net.Indyuce.mmoitems.stat.type.BooleanStat;
 import org.bukkit.Material;
 
 public class BooleanStatRegister extends BooleanStat {
-
-
-
     public BooleanStatRegister(String id, Material mat, String name, String[] lore) {
         super(id, mat, name, lore, new String[]{"all"});
     }
